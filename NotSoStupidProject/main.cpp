7c03ad5e7ea27320090d5314dcaad09ef4f87cf6
Keenan <Keenanking555@gmail.com>
2019-01-24 12:26:20 -0700

    First Commit

---
+++ NotSoStupidProject/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  NotSoStupidProject
+//
+//  Created by Davis, Keenan on 1/24/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include <iostream>
+
+int main()
+{
+    
+    
+    
+}
